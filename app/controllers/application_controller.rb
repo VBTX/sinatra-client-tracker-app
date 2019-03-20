a71acace2beeecde6041286731649e1679a89eda
VBTX <jv.svedaite@gmail.com>
2019-03-20 11:15:57 -0500

    add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,43 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
-  configure do
+ configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-  end
+    enable  :sessions
+    set :session_secret, "client_tracker"
+    register Sinatra::Flash
 
   get "/" do
-    erb :welcome
+    if logged_in?
+      redirect "/users/#{@current_user.id}"
+    else
+      erb :welcome
+  end
   end
 
+  helpers do 
+
+  	def logged_in? 
+  		!!current_user
+  	end
+
+  	def current_user
+  		@current_user  = User.find_by(id: session[:user_id])
+
+  	end
+
+    def authorized_to_edit?(client)
+      client.user == current_user
+    end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        flash[:error] = "You must be logged in to view the page."
+        redirect '/'
+      end
+    end
+  	
+  end
+end
 end
